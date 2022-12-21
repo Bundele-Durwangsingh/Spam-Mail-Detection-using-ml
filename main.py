d149622b2f09ffe41cc21e19c78ec7910163bd3d
DRAG-11 <bundeledurwang@gmail.com>
2022-12-21 13:34:33 +0530

    your msg

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# procesing the data
+raw_mail_data = pd.read_csv('mail_data.csv')
+mail_data = raw_mail_data.where((pd.notnull(raw_mail_data)),'')
+mail_data.loc[mail_data['Category'] == 'spam', 'Category',] = 0
+mail_data.loc[mail_data['Category'] == 'ham', 'Category',] = 1
+X = mail_data['Message']
+
+Y = mail_data['Category']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=3)
+# transform the text data to feature vectors that can be used as input to the Logistic regression and 
+# converting Y_train and Y_test values as integers
+feature_extraction = TfidfVectorizer(min_df = 1, stop_words='english',lowercase='True')
+
+X_train_features = feature_extraction.fit_transform(X_train)
+X_test_features = feature_extraction.transform(X_test)
+
+Y_train = Y_train.astype('int')
+Y_test = Y_test.astype('int')
+# training the Logistic Regression model with the training data
+model = LogisticRegression()
+model.fit(X_train_features, Y_train)
+msg=input("enter Your mail ==>  ")
+input_mail = ["msg"]
+input_data_features = feature_extraction.transform(input_mail)
+prediction = model.predict(input_data_features)
+print(prediction)
+if (prediction[0]==1):
+  print('Ham mail')
+else:
+  print('Spam mail')
+
+
+#I've been searching for the right words to thank you for this breather. I promise i wont take your help for granted and will fulfil my promise. You have been wonderful and a blessing at all times
